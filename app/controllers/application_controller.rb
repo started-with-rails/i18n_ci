79fa8e8214f6d7c9844939652d1afd7657328ceb
sathibabu-icentris <s.patchipala@icentris.com>
2021-03-20 14:53:39 +0530

    setup github actions, robocop and simplecov

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
@@ -7,7 +9,7 @@ class ApplicationController < ActionController::Base
     parsed_locale = params[:locale]
     I18n.locale = I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : I18n.default_locale
   end
-  
+
   def default_url_options
     { locale: I18n.locale }
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
-  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
-  validates :description, presence: true, length: {minimum: 10}
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
