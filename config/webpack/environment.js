30307cea56508b31b385dda9b0898f66f3548e83
sathibabu-icentris <s.patchipala@icentris.com>
2021-03-20 16:38:28 +0530

    rspec failuers fixes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,6 @@
-// config/webpack/environment.js
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
-
+ 
 environment.plugins.prepend(
     'Provide',
     new webpack.ProvidePlugin({
