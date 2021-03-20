29a13908b91ca5b4c5d984b8bf1831f51429e412
sathibabu-icentris <s.patchipala@icentris.com>
2021-03-20 14:25:13 +0530

    setup Internationalization multi language support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    parsed_locale = params[:locale]
+    I18n.locale = I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : I18n.default_locale
+  end
+  
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
+  validates :description, presence: true, length: {minimum: 10}
 end
